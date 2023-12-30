244c7c4ac221dff50767e6d041f3132eae682a2d
mitrofanov <83586607+exodie@users.noreply.github.com>
2023-12-30 23:29:16 +0500

    add [0.1.4] web recovery-password
    - Добавил страницу для "Восстановления пароля" (также без actions)

---
+++ web/app/auth/recovery/page.tsx
@@ -0,0 +1,19 @@
+import { Button, Input } from "@nextui-org/react";
+
+export default function AuthRecovery() {
+  return (
+    <form className="mt-16 p-10 flex w-6/12 flex-wrap md:flex-nowrap gap-4 flex-col">
+      <h1 className="font-light text-3xl mb-4 m-auto">Восстановление пароля</h1>
+      <p className="font-light mb-4 m-auto text-center">
+        Введите адрес электронной почты, привязанный к вашему
+        аккаунту, на него придет письмо со ссылкой для восстановления
+        пароля.
+      </p>
+
+      <Input isRequired type="email" label="Электронная почта" />
+      <Button className="m-auto w-8/12 font-light" color="primary" size="lg">
+        Войти
+      </Button>
+    </form>
+  );
+}
