29724caafa13e2190da2c804bfd2ffb8c4c3e49f
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-01 14:11:21 +0500

    refactor responsive catalog

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,44 +6,44 @@ export default function Home() {
     <section className="flex flex-col items-start justify-center gap-4 py-8 md:py-10">
       <h3 className={title({ size: "sm" })}>Каталог</h3>
 
-      <div className="grid grid-cols-3 gap-10">
+      <div className="grid grid-cols-3 gap-4 sm:grid-cols-1 md:grid-cols-3">
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
@@ -51,35 +51,35 @@ export default function Home() {
         />
 
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
           }
         />
         <Catalog
-          name={"Guitar"}
+          name={"Гитары"}
           href={"/catalog/guitars"}
           imgSrc={
             "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1"
