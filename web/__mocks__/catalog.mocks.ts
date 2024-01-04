efaca083426aecbcf56576e24cf491336573957f
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-05 02:47:14 +0400

    add mocks catalog

---
+++ web/__mocks__/catalog.mocks.ts
@@ -0,0 +1,51 @@
+export interface __Catalog__ {
+  id: number;
+  title: string;
+  image: string;
+}
+
+// TODO: Поменять ссылки для фото на нормальные где они пропущены
+export const __catalog__: __Catalog__[] = [
+  {
+    id: 1,
+    title: "Гитары",
+    image:
+      "https://i0.wp.com/10sguitars.com/wp-content/uploads/2021/07/10S-Guitars-V-2.png?resize=300%2C300&ssl=1",
+  },
+  {
+    id: 2,
+    title: "Гитарные струны",
+    image:
+      "https://guitar-guitar.ru/upload/iblock/762/zv5pg0dinymtvmp5i0a237c5sbg81anc.jpg",
+  },
+  {
+    id: 3,
+    title: "Гитарное усиление",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+  {
+    id: 4,
+    title: "Педали и процессоры эффектов",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+  {
+    id: 5,
+    title: "Чехлы и кейсы для гитар",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+  {
+    id: 6,
+    title: "Аксессуары для гитар",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+  {
+    id: 7,
+    title: "Комплектующие для гитар",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+  {
+    id: 8,
+    title: "Коммутация",
+    image: "https://generated.vusercontent.net/placeholder.svg",
+  },
+];
