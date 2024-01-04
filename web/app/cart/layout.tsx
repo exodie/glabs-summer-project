bf0151e64f0bf2ce840ed6cf21d9429d2d674089
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-05 02:08:38 +0400

    refactor responsive layout in cart/layout

--- web/app/cart/layout.tsx
+++ web/app/cart/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function CartLayout({ children }: { children: ReactNode }) {
   return (
-    <section className="flex flex-col w-full items-start justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col w-full items-start justify-center gap-4 py-8 md:py-10 md:ml-4 md:mr-4">
       {children}
     </section>
   );
