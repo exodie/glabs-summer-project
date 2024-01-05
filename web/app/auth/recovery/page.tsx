e5e1fcd92021b7bbe6697a95590efa258a04f528
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-05 22:24:08 +0400

    refactor responsive in recovery page

--- web/app/auth/recovery/page.tsx
+++ web/app/auth/recovery/page.tsx
@@ -2,8 +2,8 @@ import { Button, Input } from "@nextui-org/react";
 
 export default function AuthRecovery() {
   return (
-    <form className="mt-16 p-10 flex w-6/12 flex-wrap md:flex-nowrap gap-4 flex-col">
-      <h1 className="font-light text-3xl mb-4 m-auto">Восстановление пароля</h1>
+    <form className="mt-16 p-10 flex w-ful lg:w-6/12 flex-wrap md:flex-nowrap gap-4 flex-col">
+      <h1 className="font-light text-3xl mb-4 m-auto text-center">Восстановление пароля</h1>
       <p className="font-light mb-4 m-auto text-center">
         Введите адрес электронной почты, привязанный к вашему
         аккаунту, на него придет письмо со ссылкой для восстановления
