732fc2ed97723f31fff1a8168f12f56ce01ccd49
mitrofanov <83586607+exodie@users.noreply.github.com>
2024-01-06 04:33:05 +0400

    add new mocks - guitar catalog

---
+++ web/__mocks__/guitar.mocks.ts
@@ -0,0 +1,99 @@
+import {
+  ConditionGuitar,
+  type FirstSpecsOfGuitar,
+  TypesOfGuitar,
+} from "@/types";
+
+export const __guitars__: FirstSpecsOfGuitar[] = [
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+  {
+    codeOfItem: 540289,
+    picture:
+      "https://skifmusic.ru/thumbs/10/03/600x600_1_normal_ef2d4a29911f2d6b95045d148392.webp",
+    manufacturer: "Gibson",
+    type: TypesOfGuitar.ElectroGuitar,
+    condition: ConditionGuitar.VeryGood,
+    bodyShape: "Les Paul",
+    orientation: "Под правую руку",
+    numberOfStrings: 6,
+    numberOfFrets: 22,
+    beaker: 24.75,
+  },
+];
