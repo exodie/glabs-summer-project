7d47ea7407e85fe7c5df70c5b1a52ad1bf01d0ea
im <83586607+exodie@users.noreply.github.com>
2024-02-11 21:09:45 +0400

    add new __mocks__ & params to guitar

--- web/__mocks__/acoustic-guitar.mocks.ts
+++ web/__mocks__/acoustic-guitar.mocks.ts
@@ -1,11 +1,6 @@
-import {
-  ConditionGuitar,
-  type FirstSpecsOfGuitar,
-  TypesOfGuitar,
-} from "@/types";
-import { __acoustic_guitars__ } from "./acoustic-guitar.mocks";
+import { ConditionGuitar, FirstSpecsOfGuitar, TypesOfGuitar } from "@/types";
 
-export const __guitars__: FirstSpecsOfGuitar[] = [
+export const __acoustic_guitars__: FirstSpecsOfGuitar[] = [
   {
     codeOfItem: 540289,
     picture:
@@ -103,69 +98,5 @@ export const __guitars__: FirstSpecsOfGuitar[] = [
     numberOfFrets: 22,
     beaker: 24.75,
     article: "DMK319",
-  },
-];
-
-export const __catalog__guitars__ = [
-  {
-    id: 9,
-    title: "Акустические гитары",
-    type: TypesOfGuitar.AcousticGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/90/a1/400x400_1_normal_694c916e46f699a07fdfa0869728.webp",
-    catalogs: __acoustic_guitars__,
-  },
-  {
-    id: 10,
-    title: "Акустические гитары б/у",
-    type: TypesOfGuitar.AcousticGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/89/86/400x400_1_normal_5dc91d81a45e33723a232447199d.webp",
-  },
-  {
-    id: 11,
-    title: "Электроакустические гитары",
-    type: TypesOfGuitar.ElectroAcousticGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/b2/ae/400x400_1_normal_de990b2c9fb0dc4bdb6dc633c89b.webp",
-  },
-  {
-    id: 12,
-    title: "Классические гитары",
-    type: TypesOfGuitar.ClassicsGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/21/45/400x400_1_normal_2a5e7323bb71a2aac6895e0b6a27.webp",
-  },
-  {
-    id: 13,
-    title: "Электрогитары",
-    type: TypesOfGuitar.ElectroGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/58/eb/400x400_1_normal_11790dfab5ee62b6da82b92727b5.webp",
-  },
-  {
-    id: 14,
-    title: "Электрогитары б/у",
-    type: TypesOfGuitar.ElectroGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/44/e8/400x400_1_normal_332e47d4de1f3dfc2ee88da7acd9.webp",
-  },
-  {
-    id: 15,
-    title: "Полуакустические электрогитары",
-    type: TypesOfGuitar.SemiElectroGuitar,
-    image: "https://skifmusic.ru/thumbs/catalog/92/78/400x400_1_normal_44a341e6d46a6bb0a4d85c0f745f.webp",
-  },
-  {
-    id: 16,
-    title: "Бас-гитары",
-    type: TypesOfGuitar.ElectroBass,
-    image: "https://skifmusic.ru/thumbs/catalog/a1/dc/400x400_1_normal_df06a8c8edd003afdf6fa1b24ade.webp",
-  },
-  {
-    id: 17,
-    title: "Бас-гитары б/у",
-    type: TypesOfGuitar.ElectroBass,
-    image: "https://skifmusic.ru/thumbs/catalog/17/00/400x400_1_normal_663d9f91bc6ad3c431ead560d966.webp",
-  },
-  {
-    id: 18,
-    title: "Акустические бас-гитары",
-    type: TypesOfGuitar.AcousticBass,
-    image: "https://skifmusic.ru/thumbs/catalog/c4/cd/400x400_1_normal_2675200b848996e060a8f281125c.webp",
   },
 ];
