29c5fd475ad0d6f021f77399124298fd2920b4b8
im <83586607+exodie@users.noreply.github.com>
2024-03-16 18:17:33 +0400

    update href to product

--- backend/src/main/java/com/api/apiguitar/controlles/StringController.java
+++ backend/src/main/java/com/api/apiguitar/controlles/StringController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/catalog/string")
+@RequestMapping("api/product/string")
 public class StringController {
 
     @Autowired
