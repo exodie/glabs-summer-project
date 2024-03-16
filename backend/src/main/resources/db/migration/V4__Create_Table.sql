aed29454e490d084e05d4a0fe9ea2e139a820902
im <83586607+exodie@users.noreply.github.com>
2024-03-16 16:37:49 +0400

    Update V4__Create_Table.sql

--- backend/src/main/resources/db/migration/V4__Create_Table.sql
+++ backend/src/main/resources/db/migration/V4__Create_Table.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS users
     last_name  VARCHAR(255) NOT NULL,
     phone      VARCHAR(255) NOT NULL,
     username   VARCHAR(255),
-    email      VARCHAR(255) NOT NULL,
+    email      VARCHAR(255),
     password   VARCHAR(255) NOT NULL
 );
 
