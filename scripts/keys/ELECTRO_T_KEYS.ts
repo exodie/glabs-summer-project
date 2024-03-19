1de25ebf56f2879409dcab5555d9ba442f12a357
im <83586607+exodie@users.noreply.github.com>
2024-03-19 11:51:15 +0400

    add parseCharacteristics scripts

---
+++ scripts/keys/ELECTRO_T_KEYS.ts
@@ -0,0 +1,23 @@
+export enum TranslatedKeysElectro {
+    "Код товара" = "codeOfItem",
+    "Тип" = "type",
+    "Производитель" = "nameOfBrand",
+    "Состояние" = "condition",
+    "Страна производства" = "country",
+    "Форма корпуса" = "bodyShape",
+    "Ориентация" = "orientation",
+    "Количество струн" = "valueOfStrings",
+    "Количество ладов" = "valueOfFrets",
+    "Мензура, дюймов" = "widthOfUpperSill",
+    "Дека" = "deck",
+    "Гриф" = "neck",
+    "Накладка на гриф" = "fingerboard",
+    "Цвет корпуса" = "colorOfBody",
+    "Электроника" = "electronics",
+    "Звукосниматели" = "pickupsSchema",
+    "Бридж" = "bridge",
+    "Крепление грифа" = "fixingNeck",
+    "Артикул" = "article",
+    "Вес" = "weight"
+};
+
---
+++ scripts/parseCharacteristics.ts
@@ -0,0 +1,33 @@
+import { TranslatedKeysElectro } from "./keys/ELECTRO_T_KEYS";
+
+/**
+ * Функция для парсинга ключа с строкой в мн-во ключей со значениями
+ * @param characteristics
+ * @enum TranslatedKeysElectro заменить на нужный enum при надобности
+ * @returns
+ */
+function parseCharacteristics(characteristics: string) {
+  const keyValuePairs: Record<string, string> = {};
+  const regex = /\s*([^:]+):\s*([^\n\r]+)/g;
+  let match: string[] | null;
+
+  while ((match = regex.exec(characteristics)) !== null) {
+    const key = match[1].trim();
+    const value = match[2].trim();
+    const translatedKey =
+      TranslatedKeysElectro[key as keyof typeof TranslatedKeysElectro] || key;
+    keyValuePairs[translatedKey] = value;
+  }
+
+  return keyValuePairs;
+}
+
+/**
+ * Example how used
+const newArray = SELECTED_ARRAY.map(({ characteristics, ...obj }) => {
+  return {
+    ...obj,
+    characteristics: parseCharacteristics(characteristics),
+  };
+});
+ */
